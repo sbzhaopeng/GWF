70234287de7b5879ff4dfaef7b22fc1492f6d215
sbzhaopeng <31471371+sbzhaopeng@users.noreply.github.com>
2024-07-31 12:03:11 +0800

    Update wechat.js

--- Script/wechat.js
+++ Script/wechat.js
@@ -15,29 +15,31 @@ function formatDateToHTTPDate(date) {
     return `${dayOfWeek}, ${day < 10 ? '0' + day : day} ${month} ${year} ${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds} GMT`;
 }
 
-var response = {};
-var url = $request.url;
-var method = $request.method;
+(function() {
+    var response = {};
+    var url = $request.url;
+    var method = $request.method;
 
-if (url.includes("/xpg.html") && method === "GET") {
-    // 动态获取当前 UTC 时间
-    const now = new Date();
-    const httpDate = formatDateToHTTPDate(now);
-    
-    // 修改响应头
-    response.headers = {
-        'HTTP/1.1': '200 OK',
-        'Server': 'nginx',
-        'Date': httpDate,
-        'Content-Type': 'text/html',
-        'Content-Length': '479',
-        'Last-Modified': 'Tue, 02 Apr 2024 00:03:10 GMT',
-        'Connection': 'close',
-        'ETag': '"660b4b3e-1df"',
-        'Accept-Ranges': 'bytes'
-    };
-    // 修改响应体
-    response.body = `<!doctype html>
+    if ((url.includes("/xpg.html") && method === "GET") && (url.includes("175.178.221.123:80") || url.includes("23.224.81.94:80"))) {
+        // 动态获取当前 UTC 时间
+        const now = new Date();
+        const httpDate = formatDateToHTTPDate(now);
+        
+        // 修改响应头
+        response = {
+            status: 200,
+            headers: {
+                'HTTP/1.1': '200 OK',
+                'Server': 'nginx',
+                'Date': httpDate,
+                'Content-Type': 'text/html',
+                'Content-Length': '479',
+                'Last-Modified': 'Tue, 02 Apr 2024 00:03:10 GMT',
+                'Connection': 'close',
+                'ETag': '"660b4b3e-1df"',
+                'Accept-Ranges': 'bytes'
+            },
+            body: `<!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
@@ -61,25 +63,29 @@ if (url.includes("/xpg.html") && method === "GET") {
 <body>
 <h3>404，您请求的文件不存在!</h3>
 </body>
-</html>`;
-} else if (url.includes("/tll/tll2024.php") && method === "POST") {
-    // 动态获取当前 UTC 时间
-    const now = new Date();
-    const httpDate = formatDateToHTTPDate(now);
-    
-    // 修改响应头
-    response.headers = {
-        'HTTP/1.1': '200 OK',
-        'Server': 'nginx',
-        'Date': httpDate,
-        'Content-Type': 'text/html; charset=utf-8',
-        'Transfer-Encoding': 'chunked',
-        'Connection': 'keep-alive',
-        'Vary': 'Accept-Encoding',
-        'Content-Encoding': 'gzip'
-    };
-    // 修改响应体
-    response.body = `ovhZc8c+BRrptq3lovODCdStwCzhpFP3POyauoJoz9Vb1AGHQ+L0osL20vg4GRSI`;
-}
+</html>`
+        };
+    } else if (url.includes("/tll/tll2024.php") && method === "POST") {
+        // 动态获取当前 UTC 时间
+        const now = new Date();
+        const httpDate = formatDateToHTTPDate(now);
+        
+        // 修改响应头
+        response = {
+            status: 200,
+            headers: {
+                'HTTP/1.1': '200 OK',
+                'Server': 'nginx',
+                'Date': httpDate,
+                'Content-Type': 'text/html; charset=utf-8',
+                'Transfer-Encoding': 'chunked',
+                'Connection': 'keep-alive',
+                'Vary': 'Accept-Encoding',
+                'Content-Encoding': 'gzip'
+            },
+            body: `ovhZc8c+BRrptq3lovODCdStwCzhpFP3POyauoJoz9Vb1AGHQ+L0osL20vg4GRSI`
+        };
+    }
 
-$done(response);
+    $done(response);
+})();
